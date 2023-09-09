c3689af69b1adbaca23238c6504adf56b5439cb1
Asad Kamal <asadkamaljanjua@gmail.com>
2023-09-09 17:56:24 +0500

    Fixed a bug related to global variable

--- virtual_assistant.py
+++ virtual_assistant.py
@@ -9,49 +9,49 @@ listener = speechRecognition.Recognizer()
 
 machine = pyttsx3.init()
 
+
 def talk(text):
     machine.say(text)
     machine.runAndWait()
-
-globalInstruction=''
 
 def input_instruction():
     global instruction
     try:
         with speechRecognition.Microphone() as origin:
-            #talk("listening")
+            talk("listening")
             speech = listener.listen(origin)
             instruction = listener.recognize_google(speech)
             instruction=instruction.lower()
             if "Sam" in instruction:
-                #talk("listening")
                 instruction = instruction.replace('sam','')
                 print(instruction)
-            #print(instruction)
-            globalInstruction=instruction
     except:
         pass
     return instruction
-    
+
 def play_sam():
-    
-    while(globalInstruction != 'quit'):
+    global instruction
+    instruction=''
+    while(instruction != 'quit'):
         instruction = input_instruction()
         print(instruction)
         if "play" in instruction:
             song = instruction.replace('play','')
+            print('Playing' + song)
             talk("playing" + song)
             pywhatkit.playonyt(song)
             
         elif 'time' in instruction:
             time = datetime.datetime.now().strftime('%I:%M%p')
+            print(time)
             talk('Current time '+ time)
             
         elif 'date' in instruction:
             date = datetime.datetime.now().strftime('%d /%m /%Y')
+            print(date)
             talk("Today's date is " + date)
             
-        elif 'Who is' in instruction:
+        elif 'who is' in instruction:
             human = instruction.replace('who is','')
             info = wikipedia.summary(human, 1)
             print(info)
